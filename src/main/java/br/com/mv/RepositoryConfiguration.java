057c1083c27205aa97aecafd68c0d27d9fdf49f2
Jordão Macedo <jordao05@hotmail.com>
2018-02-01 17:22:22 -0300

    Expose ID for entityes

--- src/main/java/br/com/mv/RepositoryConfiguration.java
+++ src/main/java/br/com/mv/RepositoryConfiguration.java
@@ -1,4 +1,17 @@
 package br.com.mv;
 
-public class RepositoryConfiguration {
+import br.com.mv.entity.People;
+import br.com.mv.entity.Phone;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class RepositoryConfiguration extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(People.class);
+        config.exposeIdsFor(Phone.class);
+    }
 }
